5d6142c353142896000b1a7b724705e87f75626c
Roman Smirnov <roman@railsdog.com>
2010-02-22 15:47:37 +0300

    Added helper to get option values by ids.

--- sphinx_search_extension.rb
+++ sphinx_search_extension.rb
@@ -81,8 +81,21 @@ class SphinxSearchExtension < Spree::Extension
       end
     end
     
-    Spree::BaseController.class_eval do
+    ProductsController.class_eval do
       helper :sphinx
+      helper_method :option_values
+      private
+      def option_values(ids = [])
+        @option_values ||= {}
+        return @option_values if ids.empty?
+        
+        option_values_for_ids = {}
+        OptionValue.find(ids).each do |ov|
+          option_values_for_ids[ov.id] = ov.presentation
+        end
+        @option_values.merge!(option_values_for_ids)
+        return option_values_for_ids
+      end
     end
     
     Spree::Config.searcher = Spree::Search::ThinkingSphinx.new 
