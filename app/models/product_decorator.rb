927644bb779861a4c9fa3fb7766aaea6ed39f4c3
Anton Ageev <antage@gmail.com>
2011-04-13 04:50:27 +0800

    Refactored product options indexing

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,36 +1,34 @@
 Product.class_eval do
+  class_inheritable_array :indexed_options
+  self.indexed_options = []
 
-      define_index do
-        is_active_sql = "(products.deleted_at IS NULL AND products.available_on <= NOW() #{'AND (products.count_on_hand > 0)' unless Spree::Config[:allow_backorders]} )"
-        option_sql = lambda do |option_name|
-          sql = <<-eos
-            SELECT DISTINCT p.id, ov.id
-            FROM option_values AS ov
-            LEFT JOIN option_types AS ot ON (ov.option_type_id = ot.id)
-            LEFT JOIN option_values_variants AS ovv ON (ovv.option_value_id = ov.id)
-            LEFT JOIN variants AS v ON (ovv.variant_id = v.id)
-            LEFT JOIN products AS p ON (v.product_id = p.id)
-            WHERE (ot.name = '#{option_name}' AND p.id>=$start AND p.id<=$end);
-          eos
-          sql.gsub("\n", ' ').gsub('  ', '')
-        end
+  define_index do
+    is_active_sql = "(products.deleted_at IS NULL AND products.available_on <= NOW() #{'AND (products.count_on_hand > 0)' unless Spree::Config[:allow_backorders]} )"
+    option_sql = lambda do |option_name|
+      sql = <<-eos
+        SELECT DISTINCT p.id, ov.id
+        FROM option_values AS ov
+        LEFT JOIN option_types AS ot ON (ov.option_type_id = ot.id)
+        LEFT JOIN option_values_variants AS ovv ON (ovv.option_value_id = ov.id)
+        LEFT JOIN variants AS v ON (ovv.variant_id = v.id)
+        LEFT JOIN products AS p ON (v.product_id = p.id)
+        WHERE (ot.name = '#{option_name}' AND p.id>=$start AND p.id<=$end);
+        #{source.to_sql_query_range}
+      eos
+      sql.gsub("\n", ' ').gsub('  ', '')
+    end
 
-    brand_sql = option_sql.call('brand')
-    color_sql = option_sql.call('color')
-    size_sql  = option_sql.call('size')
-
-        set_property :sql_range_step => 1000000
-        indexes :name
-        indexes :description
-        indexes :meta_description
-        indexes :meta_keywords
-        indexes taxons.name, :as => :taxon, :facet => true
-        has taxons(:id), :as => :taxon_ids
-        group_by :deleted_at
-        group_by :available_on
-        has is_active_sql, :as => :is_active, :type => :boolean
-        has brand_sql , :as => :brand_option, :source => :ranged_query, :type => :multi, :facet => true
-#        has color_sql , :as => :color_option, :source => :ranged_query, :type => :multi, :facet => true
-#        has size_sql , :as => :size_option, :source => :ranged_query, :type => :multi, :facet => true
-      end
+    indexes :name
+    indexes :description
+    indexes :meta_description
+    indexes :meta_keywords
+    indexes taxons.name, :as => :taxon, :facet => true
+    has taxons(:id), :as => :taxon_ids
+    group_by :deleted_at
+    group_by :available_on
+    has is_active_sql, :as => :is_active, :type => :boolean
+    source.model.indexed_options.each do |opt|
+      has option_sql.call(opt.to_s), :as => :"#{opt}_option", :source => :ranged_query, :type => :multi, :facet => true
+    end
+  end
 end
